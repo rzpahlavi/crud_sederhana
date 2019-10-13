f2a0fbde0788acdbeb42ec3a9db8cc84abdb4414
rzpahlavi <dinus.rzpahlavi@gmail.com>
2019-10-13 21:29:25 +0700

    Tambah Index

---
+++ index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+
+    <title>Hello, world!</title>
+</head>
+
+<h1>
+    <h1>Hello, world!</h1>
+
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="assets/bootstrap/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/popper.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+</h1>
+
+</html>
